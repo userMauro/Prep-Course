47038ec34671f75763450fa30dd742c296651a0a
Mauro <mauroncioci@gmail.com>
2022-03-08 16:26:47 -0300

    homework I finished

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "modifico";
+const nuevaString = "string";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 3;
+const nuevoNum = 1;
 
 // Crea una variable booleana:
 const nuevoBool = true;
