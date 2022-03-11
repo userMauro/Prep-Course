7117de674f544f94ca75033adb6e4db7905028b0
Mauro <mauroncioci@gmail.com>
2022-03-11 12:29:06 -0300

    homework finished

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,9 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var matriz = Object.entries(objeto);
+  return matriz;
+  // Object.entries transforma un objeto en un array
 }
 
 
@@ -18,6 +21,17 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var obj = {};
+
+  for (i = 0; i < string.length; i++) {
+    if (!obj[string[i]]) obj[string[i]] = 0;
+    obj[string[i]] += 1;
+  }
+  return obj;
+  //24. declaro el objeto nuevo
+  //26. con un bucle for, recorro el string pasado por parámetro
+  //27. si no existe el obj[string[i]] en nuestro obj, lo crea con un valor de 0
+  //28. si ya existe esa clave en el 'obj', eleva su valor en 1
 }
 
 
@@ -26,6 +40,24 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayus = "";
+  var minus = "";
+
+  for (i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      mayus += s[i];
+    } else {
+      minus += s[i];
+    }
+  }
+  return (mayus + minus);
+  //43,44. creo las variables string 'mayus' y 'minus'
+  //46. uso un bucle para recorrer el string pasado por el parámetro 's'
+  //47. si la primer letra del elemento 's' es igual a la primer letra en maýuscula >>>
+  // >>> la guardo en mi variable 'mayus' junto a las mayúsculas anteriores
+  //50. si no es mayor, es menor, entonces guardo la letra en 'minus'
+  //53. devuelvo la suma de 'mayus' (primero) y 'minus' (segundo) para que me queden >>>
+  // las mayúsculas adelante en el nuevo string
 }
 
 
@@ -35,6 +67,23 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var invertida = str.split(" ").reduce(function(frase, palabras) {
+    return frase += " " + palabras.split("").reverse().join("");
+  },"")
+
+  return invertida.slice(1);
+  //70. declaro 'invertida' que tiene el argumento 'str' con 2 funciones
+  // .split(" ") devuelve una copia del string 'str' en un array separando los elementos del interior >>>
+  // >>> cuando encuentre el caracter " "
+  // .reduce() muy importante usarlo seguido de split, porque solo se aplica a un array, si lo agrego >>>
+  // >>> en otra linea sin el split, reconoce al 'str' como string y tira error
+  //71. el reduce() recorre el array 'str' elemento por elemento, guardando de a uno en 'palabras' y >>>
+  // >>> en 'frase' guarda lo que mande el 'return'
+  // palabras.split("").reverse().join("") importante que vayan todas juntas (se aplican solo a un array) >>>
+  // >>> si se aplica a un string tirra error.
+  // split("") separo el string letra por letra y creo un array. reverse() para dar vuelta la palabra >>>
+  // >>> y con join("") vuelvo a transformar el array en un string con la palabra inversa
+  //74. retorno invertida que le agrego el método slice(1) para borrar el primer carácter que es un " "
 } 
 
 
@@ -43,13 +92,20 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var string = numero.toString();
+  if (string === string.split("").reverse().join("")) return "Es capicua";
+  else return "No es capicua";
+  //95. transformo el número en un string
+  //96. comparo el string con el string pero invertido, si son iguales, es capicua
 }
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
-  //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
+  //y devuelva la versión modificada o la misma cadena, en caso de *NO* contener dichas letras.
   //Escribe tu código aquí
+  var resultado = cadena.replace(/[abc]/g, '')
+  return resultado
 }
 
 
@@ -57,6 +113,13 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  var arrayOrdenado = arr.sort(function(a, b){ return a.length - b.length})
+  return arrayOrdenado;
+// arr.sort() por defecto te ordena el array de A a Z pero tambien funciona comparando números >>>
+// >>> al agregarle una función de comparación:
+// (function(a, b) {return a.length - b.length}) 'a' y 'b' en bucle van comparando los elementos del array
+// al agregarle {return a.length - b.length} en vez de {return a - b}, me los compara por la longitud >>>
+// de sus palabras y no por orden alfabético, poner + o - cambia el orden de mayor a menor.
 }
 
 
@@ -65,7 +128,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var nuevoArray = [];
+
+  for (var i = 0; i < arreglo1.length; i++) {
+    if (arreglo2.includes(arreglo1[i])) {
+      nuevoArray.push(arreglo1[i]);
+    }
+  }
+
+  return nuevoArray;
 }
 
 
